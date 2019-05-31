d943f73d537257ac177bc143cf0442adec600d88
Pooja <pooja.pandeya123@gmail.com>
2019-05-31 18:41:49 +0530

    Basic concepts covered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    persons: [
+      { name: "Pooja", age: 28 },
+      { name: "Shivam", age: 28 }
+    ]
+  }
+
+  switchHandler = (newName1, newName2) => {
+    this.setState({
+      persons: [
+        { name: newName1, age: 28 },
+        { name: newName2, age: 28 }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Pooja", age: 28 },
+        { name: event.target.value, age: 28 }
+      ]
+    })
+  }
+
+  render() {
+    const style = {
+      backgroundColor: '#ccc',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+    return (
+      <div className="App">
+        <h1>React Guide</h1>
+        <button 
+          onClick = {this.switchHandler.bind(this, "Maximalian", "Joe")}
+           style = {style} >Switch Names</button>
+        <Person 
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age} 
+          click = {this.switchHandler.bind(this, "Pooja Pandey", "Prashant Pandey")}/>
+        <Person 
+          name= {this.state.persons[1].name} 
+          age= {this.state.persons[1].age}
+          changename = {this.nameChangeHandler} >We are married</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
